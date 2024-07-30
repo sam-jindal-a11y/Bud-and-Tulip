6c4d7aeebef2e94aab09ce18230b9f418a55d89d
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2024-07-30 19:00:20 +0530

    ss

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,9 @@ module.exports = {
     // ... other configurations ...
     devServer: {
       // ... other devServer settings ...
-      allowedHosts: ['*'], // Allow all hosts
+      allowedHosts: [
+        'all'
+      ],
     },
   };
   
