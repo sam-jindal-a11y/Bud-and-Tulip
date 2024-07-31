421ce038dee290a810ad698c0c4d87b3ab755ef4
Harsh Mangal <116105912+harsh-mangal@users.noreply.github.com>
2024-07-31 20:07:45 +0530

    pushuing

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -42,7 +42,8 @@ const imagekit = new ImageKit({
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(__dirname);
+console.log("hello");
+console.log(__dirname+ 'hello');
 
 
 const app = express();
