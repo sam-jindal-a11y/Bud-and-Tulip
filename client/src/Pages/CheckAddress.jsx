aa7354c4da94f1e68e0c6cd8dc6c5cba71038f4f
Harsh Mangal <dev.harshmangal@gmail.com>
2025-07-18 03:09:33 +0530

    fixed new code

--- client/src/Pages/CheckAddress.jsx
+++ client/src/Pages/CheckAddress.jsx
@@ -1,90 +1,142 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {jwtDecode} from 'jwt-decode';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import config from "../config";
 
 const CheckAddress = () => {
   const [address, setAddress] = useState(null);
-  const [countries, setCountries] = useState([]);
   const navigate = useNavigate();
+
+  // Static countries list with India at the top
+  const countries = [
+    "India",
+    "United States",
+    "United Kingdom",
+    "Canada",
+    "Australia",
+    "Germany",
+    "France",
+    "Italy",
+    "Spain",
+    "Netherlands",
+    "Belgium",
+    "Switzerland",
+    "Sweden",
+    "Norway",
+    "Denmark",
+    "Finland",
+    "Japan",
+    "South Korea",
+    "China",
+    "Singapore",
+    "Malaysia",
+    "Thailand",
+    "Indonesia",
+    "Philippines",
+    "Vietnam",
+    "Brazil",
+    "Argentina",
+    "Chile",
+    "Mexico",
+    "Colombia",
+    "Peru",
+    "South Africa",
+    "Nigeria",
+    "Kenya",
+    "Egypt",
+    "Morocco",
+    "United Arab Emirates",
+    "Saudi Arabia",
+    "Qatar",
+    "Kuwait",
+    "Israel",
+    "Turkey",
+    "Russia",
+    "Ukraine",
+    "Poland",
+    "Czech Republic",
+    "Hungary",
+    "Romania",
+    "Bulgaria",
+    "Greece",
+    "Portugal",
+    "Austria",
+    "Ireland",
+    "Luxembourg",
+    "New Zealand",
+  ];
 
   useEffect(() => {
     const fetchAddress = async () => {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       const decoded = jwtDecode(token);
       const userId = decoded.id;
 
       try {
-        const response = await fetch(`${config}/api/address/?userId=${userId}`, {
-          headers: {
-            'Authorization': `${token}`
+        const response = await fetch(
+          `${config}/api/address/?userId=${userId}`,
+          {
+            headers: {
+              Authorization: `${token}`,
+            },
           }
-        });
+        );
         const result = await response.json();
         if (response.ok) {
           setAddress(result[0]);
         } else {
-          alert('Error: ' + result.message);
+          alert("Error: " + result.message);
         }
       } catch (error) {
-        alert('Error: ' + error.message);
-      }
-    };
-
-    const fetchCountries = async () => {
-      try {
-        const response = await fetch('https://restcountries.com/v3.1/all'); // Example API endpoint
-        const data = await response.json();
-        setCountries(data);
-      } catch (error) {
-        console.error('Error fetching countries:', error);
+        alert("Error: " + error.message);
       }
     };
 
     fetchAddress();
-    fetchCountries();
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     const formData = new FormData(e.target);
 
     const addressData = {
-      firstName: formData.get('first-name') || '',
-      lastName: formData.get('last-name') || '',
-      company: formData.get('company') || '',
-      address1: formData.get('address1') || '',
-      address2: formData.get('address2') || '',
-      city: formData.get('city') || '',
-      country: formData.get('country') || '',
-      province: formData.get('province') || '',
-      postalCode: formData.get('postal-code') || '',
-      phone: formData.get('phone') || '',
-      defaultAddress: formData.get('default-address') === 'on',
+      firstName: formData.get("first-name") || "",
+      lastName: formData.get("last-name") || "",
+      company: formData.get("company") || "",
+      address1: formData.get("address1") || "",
+      address2: formData.get("address2") || "",
+      city: formData.get("city") || "",
+      country: formData.get("country") || "",
+      province: formData.get("province") || "",
+      postalCode: formData.get("postal-code") || "",
+      phone: formData.get("phone") || "",
+      defaultAddress: formData.get("default-address") === "on",
     };
 
     try {
-      const addressId = address ? address._id : '';
-      const method = address ? 'PUT' : 'POST';
-      const url = address ? `${config}/api/address/${addressId}` : `${config}/api/address`;
+      const addressId = address ? address._id : "";
+      const method = address ? "PUT" : "POST";
+      const url = address
+        ? `${config}/api/address/${addressId}`
+        : `${config}/api/address`;
       const response = await fetch(url, {
         method,
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `${token}`
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
         },
-        body: JSON.stringify(addressData)
+        body: JSON.stringify(addressData),
       });
       const result = await response.json();
       if (response.ok) {
         // alert('Address ' + (address ? 'updated' : 'added') + ' successfully');
-        navigate('/CheckoutPage');
+        navigate("/CheckoutPage");
       } else {
-        alert('Error: ' + result.error);
+        alert("Error: " + result.error);
       }
     } catch (error) {
-      alert('Error: ' + error.message);
+      alert("Error: " + error.message);
     }
   };
 
@@ -96,37 +148,58 @@ const CheckAddress = () => {
     <div className="addressform flex justify-center items-center min-h-screen bg-gray-100">
       <div className="flex flex-col max-w-lg w-full bg-white p-8 shadow-md rounded-lg my-20">
         <h1 className="text-2xl font-semibold mb-6 text-center">
-          {address ? 'Update Address' : 'Add New Address'}
+          {address ? "Update Address" : "Add New Address"}
         </h1>
         <form onSubmit={handleSubmit} className="space-y-4">
-          {['firstName', 'lastName', 'company', 'address1', 'address2', 'city', 'province', 'postalCode', 'phone'].map((field) => (
+          {[
+            "firstName",
+            "lastName",
+            "company",
+            "address1",
+            "address2",
+            "city",
+            "province",
+            "postalCode",
+            "phone",
+          ].map((field) => (
             <div key={field}>
-              <label htmlFor={field} className="block text-sm font-medium text-gray-700 capitalize">
-                {field.replace(/([A-Z])/g, ' $1').toUpperCase()}
+              <label
+                htmlFor={field}
+                className="block text-sm font-medium text-gray-700 capitalize"
+              >
+                {field.replace(/([A-Z])/g, " $1").toUpperCase()}
               </label>
               <input
                 type="text"
                 id={field}
-                name={field.replace(/[A-Z]/g, match => '-' + match.toLowerCase())}
-                defaultValue={address ? address[field] : ''}
-                placeholder={field.replace(/([A-Z])/g, ' $1').toUpperCase()}
+                name={field.replace(
+                  /[A-Z]/g,
+                  (match) => "-" + match.toLowerCase()
+                )}
+                defaultValue={address ? address[field] : ""}
+                placeholder={field.replace(/([A-Z])/g, " $1").toUpperCase()}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               />
             </div>
           ))}
 
           <div>
-            <label htmlFor="country" className="block text-sm font-medium text-gray-700">Country</label>
+            <label
+              htmlFor="country"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Country
+            </label>
             <select
               id="country"
               name="country"
-              defaultValue={address ? address.country : ''}
+              defaultValue={address ? address.country : ""}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             >
               <option value="">Select Country</option>
               {countries.map((country) => (
-                <option key={country.cca3} value={country.name.common}>
-                  {country.name.common}
+                <option key={country} value={country}>
+                  {country}
                 </option>
               ))}
             </select>
@@ -140,7 +213,10 @@ const CheckAddress = () => {
               defaultChecked={address ? address.defaultAddress : false}
               className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
             />
-            <label htmlFor="default-address" className="ml-2 block text-sm text-gray-900">
+            <label
+              htmlFor="default-address"
+              className="ml-2 block text-sm text-gray-900"
+            >
               Set as default address
             </label>
           </div>
@@ -150,7 +226,7 @@ const CheckAddress = () => {
               type="submit"
               className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
             >
-              {address ? 'Update Address' : 'Add Address'}
+              {address ? "Update Address" : "Add Address"}
             </button>
             <button
               type="button"
